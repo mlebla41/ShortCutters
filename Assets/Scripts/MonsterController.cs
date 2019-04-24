68f33d8391687980cd35ecec93c3a06e363900ef
mlebla41 <mlebla41@student.fitchburgstate.edu>
2019-04-24 19:55:18 -0400

    Added map and scripts

--- Assets/Scripts/MonsterController.cs
+++ Assets/Scripts/MonsterController.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraController : MonoBehaviour {
+public class MonsterController : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public GameObject Player;
+    public float speed;
+
+
+	void Update () {
+
+        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0.0f);
+
+        transform.position = transform.position + movement * Time.deltaTime * speed;
+        
+
+    }
+}
