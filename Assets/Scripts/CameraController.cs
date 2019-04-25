283d8d42b34222691ba94af9819eabdb1c02d673
mlebla41 <mlebla41@student.fitchburgstate.edu>
2019-04-25 12:03:26 -0400

    Camera lerp

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -7,14 +7,16 @@ public class CameraController : MonoBehaviour
 
     public GameObject player;
     private Vector3 offset;
+    private float lorp;
     
     void Start()
     {
         offset = transform.position - player.transform.position;
+        lorp = Mathf.Lerp(0, 3, 0.2f);
     }
     
     void LateUpdate()
     {
-        transform.position = player.transform.position + offset;
+        transform.position = (player.transform.position + offset) * lorp;
     }
 }
